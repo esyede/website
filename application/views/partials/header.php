42f7686de44bff353797a79e2ce0bcd32fc60e1b
esyede <suyadi.1992@gmail.com>
2021-05-18 14:46:03 +0700

    Console: fix favicon.ico - No such file or directory

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -3,7 +3,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo e($brand);?> :: <?php echo e($page);?></title>
 	<meta name="description" content="<?php echo e($brand);?> :: <?php echo e($tagline);?>">
-	<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+	<link rel="icon" type="image/png" href="data:;base64,iVBORw0KGgo=">
 	<link rel="stylesheet" href="<?php echo asset('main/css/main.min.css?v='.RAKIT_VERSION);?>">
 	<meta property="og:type" content="website">
 	<meta property="og:title" content="<?php echo e($brand);?> :: <?php echo e($page);?>">
