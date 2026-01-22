c9a0a3fb68af6bcc32a233efb1f4e072c9bb8014
esyede <suyadi.1992@gmail.com>
2026-01-22 19:28:07 +0700

    sync

---
+++ application/language/id/home.php
@@ -0,0 +1,75 @@
+<?php
+
+defined('DS') or exit('No direct access.');
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Home Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Baris - baris bahasa berikut ini digunakan di halaman homepage
+    |
+    */
+    'navbar' => [
+        'one' => 'Rumah',
+        'two' => 'Dokumentasi',
+        'three' => 'API',
+        'four' => 'Repositori',
+        'five' => 'Forum',
+        'six' => 'Github',
+    ],
+    'hero' => [
+        'slogan' => 'Kerangka kerja PHP sederhana, ringan dan modular.',
+        'btn1' => 'Download',
+        'btn2' => 'Dokumentasi',
+        'text' => 'Rilis terbaru:',
+    ],
+    'card_1' => [
+        'head' => 'Dasar yang Kuat',
+        'text' => "Manfaatkan fitur bawaan rakit. Kembangkan aplikasi yang benar-benar hebat. Rakit memberi anda dasar yang kuat, basis kode yang matang, dan pendekatan yang masuk akal dalam menulis aplikasi web.",
+    ],
+    'card_2' => [
+        'head' => 'Mulai Dengan Cepat',
+        'text' => "Siapapun anda, pemula atau seorang programmer PHP profesional, Rakit akan membuat anda bekerja dalam waktu yang lebih singkat. Tidak ada prosedur instalasi yang berbelit-belit. Cukup unduh dan ekstrak.",
+    ],
+    'card_3' => [
+        'head' => 'Lebih Sedikit Menulis',
+        'text' => "Percepat proses pengembangan anda. Rakit sangat ringan, mudah digunakan, modular, dan cepat. Yang terpenting, rakit tidak menghalangi anda. Rakit sangat mudah dipelajari sehingga anda bisa menghemat uang untuk biaya pelatihan.",
+    ],
+    'card_4' => [
+        'head' => 'Fitur yang Lengkap',
+        'text' => "Rakit adalah alat pengembangan web yang mudah digunakan, ia datang dengan URL routing yang cepat, cache driver multi-protokol, lapisan abstraksi database yang kuat, dan dukungan untuk aplikasi multibahasa.",
+    ],
+    'card_5' => [
+        'start' => [
+            'head' => "Ayo Mulai!",
+            'text' => "Saatnya mulai mengembangkan aplikasi web dengan cara yang mudah!",
+            'btn1' => 'Download :version'
+        ],
+        'repos' => [
+            'head' => 'Repositori',
+            'text' => 'Bergabunglah bersama kami di :vcs untuk membuat Rakit semakin baik.',
+            'vcs' => 'Github',
+        ],
+        'forum' => [
+            'head' => 'Forum',
+            'text' => "Mengalami kesulitan untuk memulai? Atau ingin berbagi pemikiran? Mari berbincang dengan pengembang lain di :forum",
+            'forum' => 'Forum Diskusi',
+        ],
+    ],
+    'card_6' => [
+        'head' => 'Repositori Paket',
+        'text' => 'Download dan bagikan paket anda bersama pengembang lain',
+        'btn1' => 'Temukan Paket..',
+    ],
+    'footer' => [
+        'credit' => 'Dibuat dengan :love oleh :contributors. Dirilis dibawah :license.',
+        'contributors' => 'Para Kontributor',
+        'license' => 'Lisensi MIT',
+    ],
+    'news' => [
+        'text' => 'Ngobrol dengan pengembang lain di panel diskusi kami :more',
+        'more' => 'Selengkapnya..',
+    ],
+];
