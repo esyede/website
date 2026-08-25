51ea6b4d59d95dd926c5689daa43b36183b5de58
esyede <suyadi.1992@gmail.com>
2026-08-25 21:51:26 +0700

    sync

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,5 @@
 <?php
+
 use PhpCsFixer\Finder;
 use PhpCsFixer\Config;
 
@@ -9,7 +10,7 @@ $rules = [
     '@PSR2' => true,
     'array_syntax' => ['syntax' => 'short'],
     'binary_operator_spaces' => ['align_equals' => false, 'align_double_arrow' => false],
-    'class_keyword_remove' => true,
+    // 'class_keyword_remove' => true, // REMOVED in v3
     'cast_spaces' => true,
     'combine_consecutive_unsets' => true,
     'concat_space' => ['spacing' => 'none'],
@@ -27,27 +28,25 @@ $rules = [
     'lowercase_keywords' => true,
     'method_argument_space' => ['ensure_fully_multiline' => false],
     'method_chaining_indentation' => true,
-    'method_separation' => true,
+    'class_attributes_separation' => true, // was method_separation in v2
     'modernize_types_casting' => true,
     'normalize_index_brace' => true,
     'new_with_braces' => true,
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
     'single_blank_line_before_namespace' => false,
-    'no_blank_lines_after_class_opening' => true,
-    'no_blank_lines_after_phpdoc' => true,
     'no_break_comment' => false,
     'no_closing_tag' => true,
     'no_empty_comment' => true,
     'no_empty_statement' => true,
-    'no_extra_consecutive_blank_lines' => true,
+    'no_extra_blank_lines' => true, // was no_extra_consecutive_blank_lines in v2
     'no_leading_import_slash' => true,
     'no_mixed_echo_print' => ['use' => 'echo'],
     'no_null_property_initialization' => true,
     'no_short_echo_tag' => true,
     'no_spaces_around_offset' => true,
     'no_spaces_inside_parenthesis' => true,
-    'no_trailing_comma_in_singleline_array' => true,
+    'no_trailing_comma_in_singleline' => true, // was no_trailing_comma_in_singleline_array in v2
     'no_trailing_whitespace' => true,
     'no_trailing_whitespace_in_comment' => true,
     'no_unused_imports' => true,
@@ -55,7 +54,6 @@ $rules = [
     'no_useless_return' => true,
     'no_whitespace_in_blank_line' => true,
     'no_whitespace_before_comma_in_array' => true,
-    'no_whitespace_in_blank_line' => true,
     'not_operator_with_successor_space' => true,
     'phpdoc_align' => true,
     'phpdoc_indent' => true,
@@ -76,7 +74,7 @@ $rules = [
     'switch_case_space' => false,
     'ternary_operator_spaces' => true,
     'ternary_to_null_coalescing' => false,
-    'trailing_comma_in_multiline_array' => true,
+    'trailing_comma_in_multiline' => ['elements' => ['arrays']], // was trailing_comma_in_multiline_array in v2
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
 ];
@@ -89,7 +87,7 @@ foreach ($files as $file) {
     $finder->notName($file);
 }
 
-return Config::create()
+return (new Config())
     ->setRiskyAllowed(true)
     ->setRules($rules)
     ->setUsingCache(false)
