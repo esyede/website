804bfe67099774013a308077a2215d1e638ec8aa
esyede <suyadi.1992@gmail.com>
2026-09-17 11:13:54 +0700

    sync

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -138,7 +138,7 @@ class Home_Controller extends Controller
         Log::info('Download from: '.Request::ip());
         Log::channel(null);
 
-        return Redirect::to('https://github.com/esyede/rakit/archive/main.tar.gz');
+        return Redirect::away('https://github.com/esyede/rakit/archive/main.tar.gz');
     }
 
     /**
